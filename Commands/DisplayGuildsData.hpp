1df4e5691e7d38941c7ce41cb8b492b0dd86c84b
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-05-05 04:27:48 -0400

    Update DisplayGuildsData.hpp

--- Commands/DisplayGuildsData.hpp
+++ Commands/DisplayGuildsData.hpp
@@ -30,20 +30,11 @@ namespace DiscordCoreAPI {
 			try {
 				Channel channel = Channels::getCachedChannelAsync({ .channelId = newArgs.eventData.getChannelId() }).get();
 
-				InputEvents::deleteInputEventResponseAsync(newArgs.eventData).get();
-				Guild guild = Guilds::getCachedGuildAsync({ newArgs.eventData.getGuildId() }).get();
-				DiscordGuild discordGuild{ guild };
-				GuildMember guildMember =
-					GuildMembers::getCachedGuildMemberAsync({ .guildMemberId = newArgs.eventData.getAuthorId(), .guildId = newArgs.eventData.getGuildId() })
-						.get();
-				bool doWeHaveAdminPermission = doWeHaveAdminPermissions(newArgs, newArgs.eventData, discordGuild, channel, guildMember);
-				if (!doWeHaveAdminPermission) {
-					return;
-				}
-
 				uint32_t currentCount = 0;
 				std::vector<Guild> theCache = Guilds::getAllGuildsAsync().get();
-				InputEventData inputEvent = newArgs.eventData;
+				RespondToInputEventData dataPackage(newArgs.eventData);
+				dataPackage.setResponseType(InputEventResponseType::Ephemeral_Deferred_Response);
+				auto inputEvent = InputEvents::respondToEventAsync(dataPackage).get();
 				for (auto& value: theCache) {
 					std::string msgString = "__Guild Name:__ " + value.name + "\n";
 					msgString += "__Guild ID:__ " + value.id + "\n";
@@ -51,7 +42,7 @@ namespace DiscordCoreAPI {
 
 					msgString += "__Joined At:__ " + value.joinedAt.getDateTimeStamp(TimeFormat::LongDateTime) + "\n";
 					User owner = Users::getUserAsync({ value.ownerId }).get();
-					msgString += "__Guild Owner:__ <@" + value.ownerId + "> " + owner.userName + "#" + owner.discriminator + "\n";
+					msgString += "__Guild Owner:__ <@!" + value.ownerId + "> " + owner.userName + "#" + owner.discriminator + "\n";
 					msgString += "__Created At:__ " + value.createdAt;
 
 					EmbedData messageEmbed;
@@ -62,21 +53,10 @@ namespace DiscordCoreAPI {
 					messageEmbed.setTimeStamp(getTimeAndDate());
 					messageEmbed.setDescription(msgString);
 
-					if (currentCount == 0) {
-						RespondToInputEventData dataPackage(newArgs.eventData);
-						dataPackage.setResponseType(InputEventResponseType::Interaction_Response);
-						dataPackage.addMessageEmbed(messageEmbed);
-						inputEvent = InputEvents::respondToEventAsync(dataPackage).get();
-						RespondToInputEventData dataPackage02(newArgs.eventData);
-						dataPackage02.setResponseType(InputEventResponseType::Edit_Interaction_Response);
-						dataPackage02.addMessageEmbed(messageEmbed);
-						inputEvent = InputEvents::respondToEventAsync(dataPackage02).get();
-					} else {
-						RespondToInputEventData dataPackage(newArgs.eventData);
-						dataPackage.setResponseType(InputEventResponseType::Follow_Up_Message);
-						dataPackage.addMessageEmbed(messageEmbed);
-						InputEvents::respondToEventAsync(dataPackage).get();
-					}
+					RespondToInputEventData dataPackage02(inputEvent);
+					dataPackage02.setResponseType(InputEventResponseType::Ephemeral_Follow_Up_Message);
+					dataPackage02.addMessageEmbed(messageEmbed);
+					inputEvent = InputEvents::respondToEventAsync(dataPackage02).get();
 					currentCount += 1;
 				};
 				return;
