bdd7d3a8f7b348c03b2f75cc5b5826f88b3e3859
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-05-05 04:34:33 -0400

    Updates!

--- Commands/BotInfo.hpp
+++ Commands/BotInfo.hpp
@@ -25,11 +25,8 @@ namespace DiscordCoreAPI {
 			return std::make_unique<BotInfo>();
 		}
 
-		virtual void execute(BaseFunctionArguments& newArgs) {
+		virtual void execute(BaseFunctionArguments& argsNew) {
 			try {
-				Guild guild = Guilds::getCachedGuildAsync({ .guildId = newArgs.eventData.getGuildId() }).get();
-				DiscordGuild discordGuild{ guild };
-
 				auto guilds = Guilds::getAllGuildsAsync().get();
 				int32_t userCount{ 0 };
 				for (auto& value: guilds) {
@@ -37,20 +34,20 @@ namespace DiscordCoreAPI {
 				}
 
 				EmbedData messageEmbed;
-				messageEmbed.setAuthor(newArgs.eventData.getUserName(), newArgs.eventData.getAvatarUrl());
-				messageEmbed.setImage(newArgs.discordCoreClient->getBotUser().avatar);
+				messageEmbed.setAuthor(argsNew.eventData.getUserName(), argsNew.eventData.getAvatarUrl());
+				messageEmbed.setImage(argsNew.discordCoreClient->getBotUser().avatar);
 				messageEmbed.setColor("FEFEFE");
 				messageEmbed.setTitle("__**Bot Info:**__");
 				messageEmbed.setTimeStamp(getTimeAndDate());
 				messageEmbed.addField(
-					"__Bot Name:__", newArgs.discordCoreClient->getBotUser().userName + "#" + newArgs.discordCoreClient->getBotUser().discriminator, true);
-				messageEmbed.addField("__Bot ID:__", newArgs.discordCoreClient->getBotUser().id, true);
+					"__Bot Name:__", argsNew.discordCoreClient->getBotUser().userName + "#" + argsNew.discordCoreClient->getBotUser().discriminator, true);
+				messageEmbed.addField("__Bot ID:__", argsNew.discordCoreClient->getBotUser().id, true);
 				messageEmbed.addField("__Guild Count:__", std::to_string(guilds.size()), true);
-				messageEmbed.addField("__Created At:__", newArgs.discordCoreClient->getBotUser().createdAt, true);
+				messageEmbed.addField("__Created At:__", argsNew.discordCoreClient->getBotUser().createdAt, true);
 				messageEmbed.addField("__Serving Users:__", std::to_string(userCount), true);
 				messageEmbed.addField("__Running On:__", "[DiscordCoreAPI Bot Library](https://discordcoreapi.com)", true);
 				messageEmbed.addField("__Created By:__", "RealTime Chris#3627", true);
-				RespondToInputEventData dataPackage(newArgs.eventData);
+				RespondToInputEventData dataPackage(argsNew.eventData);
 				dataPackage.setResponseType(InputEventResponseType::Interaction_Response);
 				dataPackage.addMessageEmbed(messageEmbed);
 				auto eventNew = InputEvents::respondToEventAsync(dataPackage).get();
