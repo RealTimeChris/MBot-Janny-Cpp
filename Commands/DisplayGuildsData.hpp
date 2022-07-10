430713df47bfa2de7594ea21ea489044076facdd
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-10 09:42:50 -0400

    Update DisplayGuildsData.hpp

--- Commands/DisplayGuildsData.hpp
+++ Commands/DisplayGuildsData.hpp
@@ -14,7 +14,7 @@ namespace DiscordCoreAPI {
 		DisplayGuildsData() {
 			this->commandName = "displayguildsdata";
 			this->helpDescription = "Displays some info about the servers that this bot is in.";
-			EmbedData msgEmbed;
+			EmbedData msgEmbed{};
 			msgEmbed.setDescription("------\nEnter /displayguildsdata.\n------");
 			msgEmbed.setTitle("__**Display Guild's Data Usage:**__");
 			msgEmbed.setTimeStamp(getTimeAndDate());
@@ -35,29 +35,29 @@ namespace DiscordCoreAPI {
 				RespondToInputEventData dataPackage(newArgs.eventData);
 				dataPackage.setResponseType(InputEventResponseType::Ephemeral_Deferred_Response);
 				auto inputEvent = InputEvents::respondToInputEventAsync(dataPackage).get();
-				for (auto& value: theCache) {
-					std::string msgString = "__Guild Name:__ " + value.name + "\n";
+				for (auto& valueNew: theCache) {
+					auto value = Guilds::getGuildAsync({ valueNew.id }).get();
+					std::string msgString = "__Guild Name:__ " + static_cast<std::string>(value.name) + "\n";
 					msgString += "__Guild ID:__ " + std::to_string(value.id) + "\n";
 					msgString += "__Member Count:__ " + std::to_string(value.memberCount) + "\n";
 
 					msgString += "__Joined At:__ " + value.joinedAt.getDateTimeStamp(TimeFormat::LongDateTime) + "\n";
-					GetUserData theData{};
-					theData.userId = value.ownerId;
-					User owner = Users::getUserAsync(theData).get();
-					msgString += "__Guild Owner:__ <@!" + std::to_string(value.ownerId) + "> " + owner.userName + "#" + owner.discriminator + "\n";
+					User owner = Users::getUserAsync({ value.ownerId }).get();
+					msgString += "__Guild Owner:__ <@!" + std::to_string(value.ownerId) + "> " + static_cast<std::string>(owner.userName) + "#" +
+						static_cast<std::string>(owner.discriminator) + "\n";
 					msgString += "__Created At:__ " + value.getCreatedAtTimestamp(TimeFormat::LongDateTime);
 
-					EmbedData messageEmbed;
-					messageEmbed.setAuthor(newArgs.eventData.getUserName(), newArgs.eventData.getAvatarUrl());
-					messageEmbed.setColor("FEFEFE");
-					messageEmbed.setThumbnail(value.icon);
-					messageEmbed.setTitle("__**Guild Data " + std::to_string(currentCount + 1) + " of " + std::to_string(theCache.size()) + "**__");
-					messageEmbed.setTimeStamp(getTimeAndDate());
-					messageEmbed.setDescription(msgString);
+					EmbedData msgEmbed{};
+					msgEmbed.setAuthor(newArgs.eventData.getUserName(), newArgs.eventData.getAvatarUrl());
+					msgEmbed.setColor("FEFEFE");
+					msgEmbed.setThumbnail(value.icon);
+					msgEmbed.setTitle("__**Guild Data " + std::to_string(currentCount + 1) + " of " + std::to_string(theCache.size()) + "**__");
+					msgEmbed.setTimeStamp(getTimeAndDate());
+					msgEmbed.setDescription(msgString);
 
 					RespondToInputEventData dataPackage02(inputEvent);
 					dataPackage02.setResponseType(InputEventResponseType::Ephemeral_Follow_Up_Message);
-					dataPackage02.addMessageEmbed(messageEmbed);
+					dataPackage02.addMessageEmbed(msgEmbed);
 					inputEvent = InputEvents::respondToInputEventAsync(dataPackage02).get();
 					currentCount += 1;
 				};
